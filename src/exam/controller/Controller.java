ade3d315bbfdeb77ede8146df42ebecd89c0be63
simjongyeon3 <simjongyeon3@gmail.com>
2023-07-10 10:22:47 +0900

    20230710 third commit

--- src/exam/controller/Controller.java
+++ src/exam/controller/Controller.java
@@ -1,5 +1,9 @@
 package exam.controller;
 
+import exam.entity.Student;
+import exam.entity.StudentEntity;
+import exam.view.StudentView;
+
 /***
  * 3Tier 아키텍처에서 client 의 request를 받는 역할을 담당.
  * client 의 해당 request 에 대한 서비스와 매핑을 해주는 역할을 담담.
@@ -8,13 +12,46 @@ package exam.controller;
  */
 public class Controller {
 	
+	// DB와 연동되는 클래스
+	StudentEntity studentEntity;
+	// 결과물 출력을 하는 클래스
+	StudentView studentView;
 	
 	// 초기화 및 샘플 데이터 등록 기능
+	public int initController(int arrLength, boolean sampleDataInsert) {
+	
+		
+		return 0;
+	}
 	
 	// Entity 초기화 
+	/***
+	 * Entity 초기화를 필요한 정보를 바탕으로 배열을 생성.
+	 * @param arrLength 생성할 배열의 길이
+	 * @param sampleDataInser 샘플데이터를 insert 할 건지의 여부
+	 */
+	public void initEntity(int arrLength, boolean sampleDataInsert) {
+		
+	}
 	
 	// 전제 등록된 데이터 출력 기능
+	/***
+	 * 등록된 모든 학생의 성적정보를 출력
+	 */
+	public void searchAllStudent() {
+		
+	}
 	
 	// 한 건의 데이터가 저장되는 기능
+	/***
+	 * 학생 한 명의 데이터를 저장.
+	 * @param stu 학생 한 명의 데이터.
+	 * @return 배열의 저장 공간이 없는 경우의 상수값
+	 */
+	public int insertStudent(Student stu) {
+		int result = 0;
+		
+		return result;
+	}
 	
 }
--- src/exam/entity/Student.java
+++ src/exam/entity/Student.java
@@ -5,4 +5,61 @@ package exam.entity;
  */
 public class Student {
 
+	int stuNo;
+	String stuName;
+	int korScore;
+	int engScore;
+	int mathScore;
+	
+	public int getStuNo() {
+		return stuNo;
+	}
+	
+	public void setStuNo(int stuNo) {
+		// this.stuNo : this 는 인스턴스 변수를 나타냄.
+		// stuNo : 매개변수를 나타냄.
+		this.stuNo = stuNo;
+	}
+
+	public String getStuName() {
+		return stuName;
+	}
+
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+
+	public int getKorScore() {
+		return korScore;
+	}
+
+	public void setKorScore(int korScore) {
+		this.korScore = korScore;
+	}
+
+	public int getEngScore() {
+		return engScore;
+	}
+
+	public void setEngScore(int engScore) {
+		this.engScore = engScore;
+	}
+
+	public int getMathScore() {
+		return mathScore;
+	}
+
+	public void setMathScore(int mathScore) {
+		this.mathScore = mathScore;
+	}
+	
+	// 총점 구하는 메소드
+	public int getTotScore() {
+		return korScore + engScore + mathScore;
+	}
+	
+	// 평균 구한는 메소드
+	public float getAvgScore() {
+		return getTotScore()/3.0f;
+	}
 }
