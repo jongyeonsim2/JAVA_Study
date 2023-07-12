1e89b460a67eafde0be152e8af1e80d479934f89
simjongyeon3 <simjongyeon3@gmail.com>
2023-07-12 17:21:40 +0900

    20230712 8th commit

--- src/ch7/PolyArgumentTest2.java
+++ src/ch7/PolyArgumentTest2.java
@@ -1,11 +1,17 @@
 package ch7;
 
+//Vector 를 사용하기 위함.
+import java.util.*;
+
+
 /*
  * 중요한 Point
  * 
  * 1. 다형성이 적용된 곳
  *    1.1 구매 메소드 => 메소드의 매개변수
  *    1.2 구매한 물건을 저장 및 관리하는 곳 => 배열에 적용됨.
+ *    1.3 구매한 물건을 저장 및 관리하는 곳 => Vector 추가 적용.
+ *    1.4 환불 메소드 => 메소드의 매개변수
  * 2. 상속 관계, 부모 class, 자식 class 확인
  * 3. 오버라이드가 된 곳
  * 
@@ -67,6 +73,14 @@ class Buyer2 {
 	// 구매한 물건을 관리하는 배열
 	Product2[] item = new Product2[10]; // 다형성 적용.
 	
+	
+	// 배열에서 Vector 로 변경.
+	Vector vec_item = new Vector();
+	
+	
+	
+	
+	
 	// 배열의 idx 를 관리
 	int i = 0;
 	
@@ -92,8 +106,29 @@ class Buyer2 {
 		// 구매한 전자제품을 목록(객체 배열)에 추가
 		item[i++] = p;
 		
+		
+		// 구매한 전자제품을 vecotr에 저장
+		vec_item.add(p);
+		
+		
 		System.out.println(p + " 을 구입."); //=> 실제 자식 class에서 toString() 오버라이딩 해야 함.
 	}
+	
+	
+	// 환불 메소드 신규 추가
+	void refund(Product2 p) {
+		if(vec_item.remove(p)) { // 정상 삭제 되면 true
+			money += p.price;
+			bonusPoint -= p.bonusPoint;
+			System.out.println(p + " 제품이 반품 완료됨.");
+		} else {
+			System.out.println("환불하려는 제품이 목록에 없음.");
+		}
+	}
+	
+	
+	
+	
 	
 	// 신규 추가 메소드
 	// 구매한 전자제품 목록 출력
@@ -103,6 +138,27 @@ class Buyer2 {
 		
 		int sum = 0;
 		String itemList = "";
+		
+		// 등록된 제품이 없는 경우.
+		if(vec_item.isEmpty()) {
+			System.out.println("구입한 제품이 없음");
+			return;
+		}
+		
+		for (int i = 0; i<vec_item.size(); i++) {
+			//vector에서 가져온 후 down castring 함.
+			Product2 p = (Product2)vec_item.get(i);
+			//총 구매 금액 계산
+			sum += p.price;
+			
+			//구입한 전자제품 리스트
+			//자식의 오버라이드된 toStirng() 호출
+			//3항식 사용
+			itemList += (  (i==0) ? "" + p : ", " + p  );  // Tv  (i==0)
+															// , Computer  (i==1)
+															// , Audio   (i==2)
+		}
+		
 		
 		for(int i=0; i<item.length; i++) {
 			// 배열의 요소가 null 경우를 반드시 체크 
@@ -138,3 +194,9 @@ public class PolyArgumentTest2 {
 	}
 
 }
+
+
+
+
+
+
