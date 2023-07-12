bbebf9b33720df36b0c571b67b0dd7fe63958e7f
simjongyeon3 <simjongyeon3@gmail.com>
2023-07-12 14:38:32 +0900

    20230712 7th commit

---
+++ src/ch7/PolyArgumentTest2.java
@@ -0,0 +1,140 @@
+package ch7;
+
+/*
+ * 중요한 Point
+ * 
+ * 1. 다형성이 적용된 곳
+ *    1.1 구매 메소드 => 메소드의 매개변수
+ *    1.2 구매한 물건을 저장 및 관리하는 곳 => 배열에 적용됨.
+ * 2. 상속 관계, 부모 class, 자식 class 확인
+ * 3. 오버라이드가 된 곳
+ * 
+ */
+
+
+//부모 class
+class Product2 {
+	int price;
+	int bonusPoint;
+	
+	//생성자
+	Product2(int price) {
+		this.price = price;
+		bonusPoint = (int)(price/10.0);
+	}
+}
+
+//자식 class
+class Tv5 extends Product2 {
+	Tv5() {
+		super(100);
+	}
+
+	// Buyer class의 buy() 메소드에서 제품 구입이 완료되었을 경우, 제품명을 출력하기 위함.
+	@Override
+	public String toString() {
+		return "Tv";
+	}
+}
+
+class Computer2 extends Product2 {
+	Computer2() {
+		super(200);
+	}
+
+	// Buyer class의 buy() 메소드에서 제품 구입이 완료되었을 경우, 제품명을 출력하기 위함.
+	@Override
+	public String toString() {
+		return "Computer";
+	}
+}
+
+// 새로운 전자 제품 추가
+class Audio extends Product2 {
+	Audio() { super(50); }
+	
+	public String toString() {
+		return "Audio";
+	}
+}
+
+//구매자 class
+class Buyer2 {
+	int money = 1000;
+	int bonusPoint = 0;
+	
+	// 1.2 에 해당
+	// 구매한 물건을 관리하는 배열
+	Product2[] item = new Product2[10]; // 다형성 적용.
+	
+	// 배열의 idx 를 관리
+	int i = 0;
+	
+	/* 별표 100000000 개  */
+	/*
+	 * 다형성을 메소드의 매개변수에 적용
+	 * 
+	 * 다형성을 적용하지 않았다면, Tv, Computer 를 구입하는 buy() 메소드가 각각 있어야 함으로,
+	 * 총 2개의 buy()을 구현해야 함.
+	 * 
+	 * 하지만, 다형성이 적용되어서 부모인 Product type의 buy() 메소드 하나만 구현하면 됨.
+	 */
+	void buy(Product2 p) {
+		if(money < p.price) {
+			System.out.println("잔액 부족. 구매 불가.");
+			return;
+		}
+		
+		money -= p.price;
+		bonusPoint += p.bonusPoint;
+		
+		// 신규 추가 코드
+		// 구매한 전자제품을 목록(객체 배열)에 추가
+		item[i++] = p;
+		
+		System.out.println(p + " 을 구입."); //=> 실제 자식 class에서 toString() 오버라이딩 해야 함.
+	}
+	
+	// 신규 추가 메소드
+	// 구매한 전자제품 목록 출력
+	void summary() {
+		// 총 구매 금액 출력
+		// 구입한 전자제품 리스트 출력 => 문자열 => String
+		
+		int sum = 0;
+		String itemList = "";
+		
+		for(int i=0; i<item.length; i++) {
+			// 배열의 요소가 null 경우를 반드시 체크 
+			// 요소가 null 이면 NullPointerException 이 발생. => 프로그램이 비정상 종료.
+			if(item[i]==null) break;
+			
+			//총 구매 금액 계산
+			sum += item[i].price;
+			
+			//구입한 전자제품 리스트
+			//자식의 오버라이드된 toStirng() 호출
+			itemList += item[i] + ",";
+		}
+		
+		// 총 구매 금액 출력
+		System.out.println("전자제품의 총 금액은 " + sum + " 입니다.");
+		// 구입한 전자제품 리스트 출력
+		System.out.println("구입한 제품은 " + itemList + " 입니다.");
+	}
+}
+
+public class PolyArgumentTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Buyer2 b = new Buyer2();
+		
+		b.buy(new Tv5());
+		b.buy(new Computer2());
+		b.buy(new Audio());
+		
+		b.summary();
+	}
+
+}
