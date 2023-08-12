0ca98b3b59a412b1250ff60ac3d5561972a7d555
simjongyeon3 <simjongyeon3@gmail.com>
2023-08-12 12:46:34 +0900

    20230812 11st commit

---
+++ src/ch14/LambdaEx6.java
@@ -0,0 +1,13 @@
+package ch14;
+
+import java.util.function.*;
+import java.util.*;
+
+public class LambdaEx6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
